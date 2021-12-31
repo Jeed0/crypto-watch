250b7b104ca283b17ec2a91c48cf6beba70fa7d1
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2021-12-31 12:46:48 -0400

    fetch API nomics.com

--- pages/index.js
+++ pages/index.js
@@ -7,3 +7,19 @@ export default function Home() {
     </Layout>
   )
 }
+
+
+// fetch by server, rendered on a static generation
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch(
+      "https://api.nomics.com/v1/currencies/ticker?key=a76a344ed6c9b0bb167f3b7efe6f6aaa&ids=BTC,ETH,XRP&interval=1d,30d,365d"
+    ).then((res) => res.json());
+
+    return {
+      props: { res },
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
