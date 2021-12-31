e6594c7f82877467e75367ff4ccc8fe968aa5cab
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2021-12-31 11:44:17 -0400

    debug tailwind

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,18 @@
 // == component NEXT
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Layout({ children, page }) {
     return (
       <div className="bg-blue-50 pt-5 text-center min-h-screen">
         <Head>
                 <title>{page}</title>
-            </Head>
+        </Head>
+        <header className="container-lg">
+        <h1 className="text-5xl mb-2">CRYPTO WATCH</h1>
+        <div className="inline-grid gird-cols-2 gap-x-10 p-4">
+        </div>
+        </header>
             {children}
         </div>
     );
