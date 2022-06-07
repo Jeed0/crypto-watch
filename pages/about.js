512bc512df3fcbab23ae8314ffa83ef86a23b4cd
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2022-06-07 09:41:35 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ export default function About() {
       <h1 className="text-4xl">A propos</h1>
       <br/>
         <p>
-            Ce site a été réalisé par Jeed0,  développeur Front junior grace au Mentoring FromScratch . C est un premier un site web avec NEXTjs, prenant en compte une API pour faire des pages dynamiques et deployé par Vercel. IL se veut être un support de travail pour s exercer.
+            Ce site a été réalisé par Jeed0,  développeur Front junior grace au Mentoring FromScratch . C est un premier un site web avec NEXTjs, prenant en compte une API pour faire des pages dynamiques et deployé par Vercel. IL se veut être un support de travail pour s exercer à récupérer de la donnée, il n est pas responsive et n a aucun intérêt esthétique .
         </p>
     </Layout>
   )
